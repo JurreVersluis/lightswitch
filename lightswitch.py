9c0a2c1d44bb59a6d414e48a554d9a826691aa59
Idioot <99066617@mydavinci.nl>
2022-01-26 09:54:37 +0100

    'lichtknop'

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,26 @@
 import tkinter as tk
-window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+window = tk.Tk(className='De lichtknop!')
+window.configure(bg='black')
+window.geometry("300x250")
 
-# schijf hier tussen je code
+welkNummer = 1
+color = ('black', 'yellow')
+aanOfUit = ('Het licht is uit!', 'Het licht is aan!')
 
 
+def Myclick():
+    global welkNummer
+    window.configure(bg=color[welkNummer])
+    button['text'] = aanOfUit[welkNummer]
+    print(aanOfUit[welkNummer])
 
-# schijf hier tussen je code
+    welkNummer += 1
+    if welkNummer > 1:
+        welkNummer = 0
+
+
+button = tk.Button(text='Het licht is uit!', bg="white", fg="black", width=15, height=5, command=Myclick)
+button.pack(pady=50, padx=50)
 
 window.mainloop()
